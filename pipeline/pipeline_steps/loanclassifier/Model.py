0ce34946090c639e06c4093b06baa461923d4feb
Alejandro Saucedo <axsauze@gmail.com>
2019-07-13 09:57:41 +0100

    added rest of edits

---
+++ pipeline/pipeline_steps/loanclassifier/Model.py
@@ -0,0 +1,14 @@
+import dill
+
+class Model:
+    def __init__(self, *args, **kwargs):
+        
+        with open("preprocessor.dill", "rb") as prep_f:
+            self.preprocessor = dill.load(prep_f)
+        with open("model.dill", "rb") as model_f:
+            self.clf = dill.load(model_f)
+        
+    def predict(self, X, feature_names=[]):
+        X_prep = self.preprocessor.transform(X)
+        proba = self.clf.predict_proba(X_prep)
+        return proba
